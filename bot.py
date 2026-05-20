d10a60c7c80cc0ca174ab12f085040bd3119b70c
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-20 20:09:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,35 @@
-import asyncio
-from pyrogram import Client, idle
-from config import API_ID, API_HASH, BOT_TOKEN
+from pyrogram import idle
+from info import ADMINS   # or config.py if ADMINS is there
+from pyrogram import Client
 
 app = Client(
-    "KTUStudyBot",
+    "KTU",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
     plugins=dict(root="plugins")
 )
 
-async def main():
-    await app.start()
-    print("✅ Bot Started")
-    await idle()
-    await app.stop()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def main():
+
+    await app.start()
+
+    print("✅ Bot Started")
+
+    for admin in ADMINS:
+
+        try:
+
+            await app.send_message(
+                admin,
+                "🤖 KTU Study Bot Started Successfully ✅"
+            )
+
+        except Exception as e:
+            print(e)
+
+    await idle()
+
+
+app.run(main())
