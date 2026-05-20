1c2fc079955fc8dcf8455d7b926e03cdf7de5e5c
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-20 20:11:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID","0"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = 13305226
+API_HASH = "8cde2475d6b0cb1162b89ebbac71a95d"
+BOT_TOKEN = "8883412478:AAFeadXD6GGuSvnnpKctCEwWmWleJ-wBk6I"
+
+ADMINS = [7412083181]
